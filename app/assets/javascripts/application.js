01c58eb1bc3a8a8942a00b1c473b4f90db2fd958
synthresin <synthresin@me.com>
2013-01-28 00:43:59 +0900

    sadfsaef

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require_tree ./plugins
 //= require handlebars
 //= require ember
 //= require ember-data
--- app/assets/javascripts/views/posts_view.js
+++ app/assets/javascripts/views/posts_view.js
@@ -2,7 +2,14 @@ App.PostsView = App.OneDepthView.extend({
 
 	didInsertElement:function() {
 		// 스크롤바 세팅해 줄것.
-		this.$('#scrollbar1').tinyscrollbar();	
+		this.scrollbar = this.$('.post-list-wrapper');
+		this.scrollbar.tinyscrollbar();	
+
+		//스크롤바 유지 보수는 어떻게 하나...
+		this.scrollbar.tinyscrollbar_update('relative');
+		
+		
+
 	}
 
 });
