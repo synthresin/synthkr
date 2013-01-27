f2b4217af0b9611526b2ca6fa3b8d42ef97c42f7
synthresin <synthresin@me.com>
2013-01-27 18:22:31 +0900

    emberjs bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require app
+App = Ember.Application.create();
 //= require_tree .
+//= require ember
