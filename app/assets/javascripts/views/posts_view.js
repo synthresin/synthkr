81f82fab3c4018453e3010bfdc1158c91be2d26d
synthresin <synthresin@me.com>
2013-01-28 00:44:17 +0900

    Revert "sadfsaef"
    This reverts commit 01c58eb1bc3a8a8942a00b1c473b4f90db2fd958.

--- app/assets/javascripts/views/posts_view.js
+++ app/assets/javascripts/views/posts_view.js
@@ -2,14 +2,7 @@ App.PostsView = App.OneDepthView.extend({
 
 	didInsertElement:function() {
 		// 스크롤바 세팅해 줄것.
-		this.scrollbar = this.$('.post-list-wrapper');
-		this.scrollbar.tinyscrollbar();	
-
-		//스크롤바 유지 보수는 어떻게 하나...
-		this.scrollbar.tinyscrollbar_update('relative');
-		
-		
-
+		this.$('#scrollbar1').tinyscrollbar();	
 	}
 
 });
