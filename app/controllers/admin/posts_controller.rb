11870a85e60991e89f48af9823219a8f9c136143
synthresin <synthresin@me.com>
2013-01-29 23:21:55 +0900

    포스트 C 완성

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,9 +3,39 @@ class Admin::PostsController < ApplicationController
 	layout 'admin'
 
 	def index
+		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params[:post])
 		
+		if @post.save
+			redirect_to admin_post_path(@post)
+		else
+			render 'new'
+		end
+		
+
 	end
 
 	def show
+		@post = Post.find(params[:id])
 	end
+
+	def edit
+		
+	end
+
+	def update
+	end
+
+	def destroy
+
+	end
+
+
 end
