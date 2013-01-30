4bbb3328c80781a1764033842d0937b7029c6d3f
synthresin <synthresin@me.com>
2013-01-30 17:04:44 +0900

    백엔드 뒤엎기 전

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,7 +2,6 @@ App.Router.map(function() {
 	this.resource('posts', function() {
 		this.route('new');
 		this.route('post', { path: ':post_id'} );
-
 	});
 	this.route('about');
 });
---
+++ app/assets/javascripts/views/posts/new_view.js
@@ -0,0 +1,3 @@
+App.PostsNewView = Ember.View.extend({
+	 classNames: ['post']
+});
