d54101291c308dbb83e404fdcf15ac2f3d9b67c5
synthresin <synthresin@me.com>
2013-01-30 16:01:53 +0900

    css 수정 좀만 욕심 버리고

--- app/assets/javascripts/models/post.js
+++ app/assets/javascripts/models/post.js
@@ -1,3 +1,4 @@
 App.Post = DS.Model.extend({
-	
+	title: DS.attr("string"),
+	content: DS.attr("string")
 });
--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,8 @@
 App.Router.map(function(match) {
 	this.resource('posts', function() {
+		this.route('new');
 		this.route('post', { path : '/1'});
+
 	});
 	this.route('about');
 });
