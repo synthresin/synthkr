dbb6e260e3670b8be6d30dcfda921f3b1982949e
synthresin <synthresin@me.com>
2013-01-31 12:59:14 +0900

    crud 위한 준비

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,19 @@ class PostsController < ApplicationController
 	def show
 		render :json => Post.find(params[:id])
 	end
+
+	def create
+		post = Post.new(params[:post])
+
+		# 받은 파라미터를 가지고 post 를 슥삭슥삭 지정한다.
+		if @post.save
+			# 세이브가 제대로 되었으면, 해당 post 를 json 으로 리턴한다.
+		else
+			
+		end
+
+		
+
+		render :json => Post.find(params[:id])
+	end
 end
